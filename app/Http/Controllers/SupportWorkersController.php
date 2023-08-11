378b80a1a026cf5da7cbb0047e054bdae4c3bd4f
itai chiriseri <itaineilchiriseri@gmail.com>
2023-08-11 13:33:11 +0200

    added another page to display the results that were subtracted

--- app/Http/Controllers/SupportWorkersController.php
+++ app/Http/Controllers/SupportWorkersController.php
@@ -71,24 +71,25 @@ class SupportWorkersController extends Controller
 
     public function actionRemoveEntry(Request $request)
     {
+    
+        $numOfPeople = -$request->input('num_people');
+        $date = $request->input('date');
+        $shift = $request->input('shift');
+        $totalPeopleForCurrentDay = SupportWorkers::whereDate('date', $date)->sum('num_people');
         
-        $dateToSubtract = $request->input('date');
-        $numOfPeopleToSubtract = -$request->input('num_people');
-        $shiftToSubtract = $request->input('shift');
-
-        $totalPeopleForCurrentDay = SupportWorkers::whereDate('date', $dateToSubtract)->sum('num_people');
-
+        // Recalculate the shift counts for the selected shift type
         $shiftCounts = DB::table('support_workers')
-        ->select(
-            DB::raw('SUM(CASE WHEN shift = "morning" THEN num_people ELSE 0 END) as morningshift'),
-            DB::raw('SUM(CASE WHEN shift = "night" THEN num_people ELSE 0 END) as nightshift'),
-            'date'
-        )
-        ->whereDate('date', '>=', $dateToSubtract)
-        ->groupBy('date')
-        ->get();
-        dd($shiftCounts);
-        return view('viewResults', ['shiftCounts' => $shiftCounts])->with("totalJobs",$totalPeopleForCurrentDay)->with("total",$totalPeopleForCurrentDay);
+            ->select(
+                DB::raw('SUM(CASE WHEN shift = ? THEN num_people + ? ELSE 0 END) as selected_shift'),
+                'date'
+            )
+            ->where('shift', $shift)  // Filter for the selected shift type
+            ->whereDate('date', '>=', $date)
+            ->groupBy('date')
+            ->setBindings([$shift, $numOfPeople], 'select')
+            ->get();
+            dd($shiftCounts);
+            return view('viewUpdatedResults', ['shiftCounts' => $shiftCounts])->with("totalJobs",$totalPeopleForCurrentDay)->with("total",$totalPeopleForCurrentDay); 
     
     }
     /**
---
+++ resources/views/viewUpdated.blade.php
@@ -0,0 +1,96 @@
+<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+<style>
+    .ribbon-wrapper {
+        position: absolute;
+        right: -10px;
+        top: -10px;
+        overflow: hidden;
+        width: 100px; /* Adjust width for longer text */
+        height: 75px;
+    }
+
+    .ribbon {
+        font-size: 1rem;
+        position: absolute;
+        padding: 6px 15px;
+        background-color: #007bff;
+        color: #fff;
+        text-transform: uppercase;
+        transform: rotate(45deg);
+        right: -45px; /* Adjust right position for longer text */
+        top: 20px;
+        white-space: nowrap; /* Prevent text from wrapping */
+    }
+
+    .info-box {
+        background: #f4f6f9;
+        border-radius: 8px;
+        border: 1px solid #d2d6de;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        overflow: hidden;
+        position: relative;
+    }
+
+    .info-box-content {
+        padding: 10px;
+    }
+
+    .info-box-text {
+        font-size: 1.25rem;
+        font-weight: 600;
+    }
+
+    .info-box-number {
+        font-size: 1rem;
+        color: #6c757d;
+    }
+
+    .info-box-icon {
+        background: #007bff;
+        color: #fff;
+        border-radius: 50%;
+        font-size: 2rem;
+        padding: 10px;
+    }
+</style>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="jumbotron">
+                <h1 class="display-4">Support Workers</h1>
+                <hr class="my-4">
+                <p>Total People For Today {{$total}}</p>
+                <a href="/" class="btn btn-success">Add Entry</a>
+                <a href="/removeEntry" class="btn btn-success">Remove Entry</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        @foreach ($shiftCounts as $shiftCount)
+        <div class="col-md-4">
+            <div class="info-box">
+                <div class="ribbon-wrapper">
+                    <div class="ribbon bg-primary">
+                        WCTC
+                    </div>
+                </div>
+                <span class="info-box-icon bg-primary"><i class="fa fa-users"></i></span>
+                <div class="info-box-content">
+            <h5 class="card-title">{{ date('Y-m-d (D)', strtotime($shiftCount->date)) }}</h5>
+            <p class="card-text">Day of the week: {{ date('l', strtotime($shiftCount->date)) }}</p>
+            <p class="card-text">Morning Shift: {{ $shiftCount->morningshift }}</p>
+            <p class="card-text">Night Shift: {{ $shiftCount->nightshift }}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            
+        </div>
+    </div>
+</div>
