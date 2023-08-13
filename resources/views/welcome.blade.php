d491c576fb894fe7d7a5b9e42b0148e3ebf66126
itai chiriseri <itaineilchiriseri@gmail.com>
2023-08-13 14:28:49 +0200

    added login button to the application

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -312,12 +312,7 @@
             <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
               <img src="{{ asset('img/logo.png') }}" alt="Profile" class="rounded-circle">
               <h2>We Choose To Care</h2>
-              <div class="social-links mt-2">
-                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-              </div>
+              <a class="btn btn-info" href="/login">Login Admin</a>
             </div>
           </div>
 
