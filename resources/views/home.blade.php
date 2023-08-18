677bcfa1f29db8567be5c0754d3dc706b0f43a6f
itai chiriseri <itaineilchiriseri@gmail.com>
2023-08-18 16:24:22 +0200

    removed search icon from the landing pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,9 +51,7 @@
       <ul class="d-flex align-items-center">
 
         <li class="nav-item d-block d-lg-none">
-          <a class="nav-link nav-icon search-bar-toggle " href="#">
-            <i class="bi bi-search"></i>
-          </a>
+        
         </li><!-- End Search Icon-->
 
         <li class="nav-item dropdown">
--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,6 @@ use App\Http\Controllers\MentalHealthCareAssistantsController;
 use App\Http\Controllers\RGNController;
 use App\Http\Controllers\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/',function(){
     return view("welcome");
 });
@@ -40,3 +29,4 @@ Route::get('removeEntry', [SupportWorkersController::class, 'removeEntry'])->nam
 Route::post('removeEntry', [SupportWorkersController::class, 'actionRemoveEntry'])->name("removeEntry");
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
